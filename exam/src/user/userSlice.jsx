53898cc82e66748ca429d718c748bdba6a8288aa
Bilolini <bilolmeliboyevharvard@gmail.com>
2023-06-12 10:24:43 +0500

    create reducers and initial state value

--- exam/src/user/userSlice.jsx
+++ exam/src/user/userSlice.jsx
@@ -3,15 +3,43 @@ import { createSlice } from '@reduxjs/toolkit'
 export const userSlice = createSlice({
     name: 'user',
     initialState: {
-        value: []
+        value: [
+            {
+                name: 'Hulk',
+                level: 'Middle',
+                field: 'Frontend',
+                prefer: [
+                    'algorithms', 'data structures'
+                ],
+                reading: [],
+                hobby: ['cooking', "videogames"]
+            },
+            {
+                name: 'Steve',
+                level: 'Founder',
+                field: 'Technology',
+                prefer: [
+                    'animation', 'product'
+                ],
+                reading: [],
+                hobby: ['calligraphy', 'startup']
+            }
+        ]
     },
     reducers: {
-        // increment: state => {
-        //     state.value += 1
-        // }
+        addUser: (state, {payload}) => {
+            state.value.push(payload)
+        },
+        updateUser: (state, {payload}) => {
+            const {id} = payload;
+            state.value[id] = payload;
+        },
+        deleteUser: (state, {payload}) => {
+            state.value = state.value.filter(elem => elem.id !== payload)
+        }
     }
 })
 
-export const { } = userSlice.actions
+export const {addUser, updateUser, deleteUser } = userSlice.actions
 
 export default userSlice.reducer;
